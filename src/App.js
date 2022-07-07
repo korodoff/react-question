e36fafecc4d174f90bcf8b77b22ad0f87e291f8f
korodoff <rudoffsutnga368@gmail.com>
2022-07-07 11:30:06 +0530

    function and class component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ClassComp from "./day1/classComp"
+import FunctionalComp from "./day1/functionalComp"
+
+function App() {
+  return (
+    <div className="App">
+      <FunctionalComp/>
+      <ClassComp/>
+    </div>
+  );
+}
+
+export default App;
